deea8d702a64c67b09c0726d9ac91c8522996a53
Tamas Michelberger <tomi.michel@gmail.com>
2013-12-06 13:26:08 +0100

    test for registering twice

--- test/command_test.c
+++ test/command_test.c
@@ -39,3 +39,26 @@ void test_user_reg_with_without_args() {
     free(res.msg);
     free(cmd.args);
 }
+
+void test_user_reg_twice() {
+    mem_init();
+    command_t cmd;
+    cmd.code = CMD_REG;
+    cmd.args = calloc(sizeof(char), sizeof(char) * 5);
+    strcpy(cmd.args, "test");
+
+    command_t cmd2;
+    cmd2.code = CMD_REG;
+    cmd2.args = calloc(sizeof(char), sizeof(char) * 5);
+    strcpy(cmd2.args, "test");
+
+    command_result_t res1 = user_reg(&cmd);
+    command_result_t res2 = user_reg(&cmd2);
+
+    assert_eq_int(CMD_RES_ERR, res2.code);
+
+    free(res1.msg);
+    free(res2.msg);
+    free(cmd.args);
+    free(cmd2.args);
+}
